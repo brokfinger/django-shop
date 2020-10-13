699f261febb5cbbd0cb19d9d5059e3f4d31c2428
Akira Shirase <51890190+brokfinger@users.noreply.github.com>
2020-10-13 15:48:01 +0300

    Added new app ordering and 2 models for bucket.

--- myshop/urls.py
+++ myshop/urls.py
@@ -19,9 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
-    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
